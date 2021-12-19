f538d899c74c501638c7b02235b0dba65e4de6c7
Prashant <prashantshrivastava118@gmail.com>
2021-12-19 14:49:27 +0530

    ini

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -8,7 +8,7 @@ function Login() {
   };
 
   const responseGoogleSuccess = (data) => {
-    // make alert to indicate login is complete and then redirect
+    // store login data in db TODO
     console.log(data);
   };
 
@@ -87,8 +87,3 @@ function Login() {
 }
 
 export default Login;
-
-//     <h5 className="card-title">Success card title</h5>
-//     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//   </div>
-// </div>
